68e8dbb1df18066c316377d1a7eedec8f44e8903
Christoph <c-alias-mail@posteo.de>
2025-07-31 14:14:46 +0200

    add documentation for gpkernel

--- cppgp/kernels/gpkernel.cpp
+++ cppgp/kernels/gpkernel.cpp
@@ -1,3 +1,27 @@
+/*
+* This file is part of the cppgp package.
+* Copyright (c) 2025 kaltertee
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the 'Software'), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
+
 #include <cppgp/kernels/gpkernel.hpp>
 
 
@@ -5,16 +29,20 @@ gp::kernel::GPKernel::GPKernel(const std::shared_ptr<gp::kernel::CovarianceFunct
     covfun(covfun), data(nullptr)
 {}
 
+
 gp::kernel::GPKernel::GPKernel(const std::shared_ptr<gp::kernel::CovarianceFunction> covfun, const double noise):
     covfun(covfun), noise(noise)
 {}
+
 
 gp::kernel::GPKernel::GPKernel(const GPKernel &gpkernel):
     covfun(gpkernel.covfun->copy()), data(gpkernel.data) //copy ref to data without copy
 {}
 
+
 gp::kernel::GPKernel::~GPKernel()
 {}
+
 
 std::shared_ptr<util::Prototype> gp::kernel::GPKernel::copy() const
 {
@@ -31,12 +59,14 @@ void gp::kernel::GPKernel::registerData(const std::shared_ptr<gp::GPData> gpdata
     this->data->subscribe(this, std::bind(&gp::kernel::GPKernel::changedData_trigger, this));
 }
 
+
 void gp::kernel::GPKernel::computeCov(Eigen::MatrixXd& K) const
 {
     Eigen::MatrixXd X;
     data->getX(X);
     covfun->K(K, X);
 }
+
 
 void gp::kernel::GPKernel::computeNoisedCov(Eigen::MatrixXd &K) const
 {
@@ -47,12 +77,14 @@ void gp::kernel::GPKernel::computeNoisedCov(Eigen::MatrixXd &K) const
     K = K + this->noise*Eigen::MatrixXd(n, n);
 }
 
+
 void gp::kernel::GPKernel::computeCrossCov(Eigen::MatrixXd& K, const Eigen::MatrixXd& X2) const
 {
     Eigen::MatrixXd X1;
     data->getX(X1);
     covfun->K(K, X1, X2);
 }
+
 
 void gp::kernel::GPKernel::computeCovDiag(Eigen::VectorXd& K) const
 {
@@ -61,10 +93,12 @@ void gp::kernel::GPKernel::computeCovDiag(Eigen::VectorXd& K) const
     this->covfun->diagK(K, X);
 }
 
+
 void gp::kernel::GPKernel::getAlpha(Eigen::VectorXd& alpha) const
 {
     alpha = this->alpha;
 }
+
 
 /*
 void gp::kernel::GPKernel::getNoisedInvCov(Eigen::MatrixXd& ICov) const
@@ -73,10 +107,12 @@ void gp::kernel::GPKernel::getNoisedInvCov(Eigen::MatrixXd& ICov) const
 }
 */
 
+
 void gp::kernel::GPKernel::getNoisedInvCov(Eigen::MatrixXd& ICov, const Eigen::MatrixXd &B) const
 {
     ICov = this->noisedCovDecomp.solve(B);
 }
+
 
 double gp::kernel::GPKernel::computeNoisedLogDetCov() const
 {
@@ -91,11 +127,13 @@ void gp::kernel::GPKernel::setParameters(const Eigen::VectorXd& params, const un
     changedData_trigger();
 }
 
+
 void gp::kernel::GPKernel::getParameters(Eigen::VectorXd& params, const unsigned int index) const
 {
     params(index) = this->noise;
     this->covfun->getParameters(params, index+1);
 }
+
 
 unsigned int gp::kernel::GPKernel::nParameters() const
 {
@@ -103,14 +141,27 @@ unsigned int gp::kernel::GPKernel::nParameters() const
 }
 
 
+std::shared_ptr<gp::GPData> gp::kernel::GPKernel::getData() const
+{
+    return this->data;
+}
+
+
+int gp::kernel::GPKernel::getN() const
+{
+    return this->data->getN();
+}
+
+
 void gp::kernel::GPKernel::precomputeAlpha()
 {
-    auto data = this->data->getData();
+    auto data = this->data->getNormalizedData();
     Eigen::MatrixXd K;
-    this->computeCov(K);
+    this->computeNoisedCov(K);
     noisedCovDecomp = K.ldlt();
-    alpha = noisedCovDecomp.solve(std::get<1>(data)); //TODO use normalized Y here?
+    alpha = noisedCovDecomp.solve(std::get<1>(data));
 }
+
 
 void gp::kernel::GPKernel::changedData_trigger()
 {
--- cppgp/kernels/gpkernel.hpp
+++ cppgp/kernels/gpkernel.hpp
@@ -1,3 +1,28 @@
+/*
+* This file is part of the cppgp package.
+* Copyright (c) 2025 kaltertee
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the 'Software'), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+*/
+
+
 #pragma once
 
 #include <memory>
@@ -10,52 +35,183 @@
 
 namespace gp::kernel {
 
-class GPKernel : public util::IObserver, public util::Prototype {
-    /**
-     * @brief Abstract kernel class.
-     *
+
+/**
+     * Kernel class for Gaussian processes based on a given covariance function.
+     * Receives notifications from the GPData class as soon as the data changes.
      */
+class GPKernel : public util::IObserver, public util::Prototype {
+
     public:
     /**
-     * @brief Create new GPKernel object.
+     * Create a new GPKernel object.
+     *
+     * @param covfun The covariance function that defines the behavior of the new kernel object.
      */
     GPKernel(const std::shared_ptr<gp::kernel::CovarianceFunction>& covfun);
 
     /**
-     * @brief Create a new GPKernel object using the given parameters.
-     * @param Parameters to initialize the GP kernel.
+     * Create a new GPKernel object using the given noise parameter.
+     *
+     * @param covfun The covariance function that defines the behavior of the new kernel object.
+     * @param noise The noise value sigma of the kernel.
      */
     GPKernel(const std::shared_ptr<gp::kernel::CovarianceFunction> covfun, const double noise);
 
     /**
-     * @brief Copy constructor to create a new GPKernel object.
-     * @param Existing GPKernel that is used to initialize the parameters.
+     * Copy constructor to create a new GPKernel object.
+     *
+     * @param gpkernel GPKernel that is copied.
      */
-    GPKernel(const GPKernel&);
+    GPKernel(const GPKernel& gpkernel);
 
     virtual ~GPKernel();
 
     /**
-     * @brief Create a deep copy of the GPKernel object.
+     * Create a deep copy of the GPKernel object.
+     *
      * @return The deep copy.
      */
     virtual std::shared_ptr<util::Prototype> copy() const override;
 
+    /**
+     * Add observation data to the kernel object.
+     *
+     * @param pgdata The new data.
+     */
     void registerData(const std::shared_ptr<gp::GPData> gpdata);
 
+    /**
+     * Compute the covariance matrix using the covariance function
+     * based on the registered data. Mathematically, it is the Gram matrix
+     * with entries \f$ K_{ij} = f_{cov}(x_i, x_j) \f$ for the covariance function \f$ f_{cov} \f$.
+     * The Gram matrix corresponding to a covariance function is
+     * - squared
+     * - symmetric
+     * - positive semi-definite
+     *
+     * [Rasmussen and Williams, p. 80].
+     *
+     * This method ignores the noise parameter.
+     *
+     * @param K Returns the covariance matrix, size [getN(), getN()].
+     */
     void computeCov(Eigen::MatrixXd& K) const;
+
+    /**
+     * Compute the covariance matrix using the covariance function
+     * based on the registered data.
+     * For more details @see computeCov.
+     *
+     * This method uses the noise parameter \f$\sigma\f$ and computes \f$(K + \sigma I)\f$.
+     *
+     * @param K Returns the covariance matrix, size [getN(), getN()].
+     */
     void computeNoisedCov(Eigen::MatrixXd& K) const;
+
+    /**
+     * Compute the cross covariance matrix using the covariance function
+     * based on the registered data and the given data.
+     *
+     * For more details @see computeCov.
+     *
+     * This method ignores the noise parameter.
+     *
+     * @param K Returns the covariance matrix, size [getN(), getN()].
+     * @param X2 The data that is used together with the registered GPData to compute the cross covariance. Size [k, getData()->getDimX()], where k is arbitrary.
+     */
     void computeCrossCov(Eigen::MatrixXd& K, const Eigen::MatrixXd& X2) const;
+
+    /**
+     * Compute the diagonal of the covariance matrix.
+     *
+     * @param K Returns the diagonal of the covariance matrix as a vector, size [getN()].
+     */
     void computeCovDiag(Eigen::VectorXd& K) const;
+
+    /**
+     * Compute the product of the inverse covariance matrix and the target observation vector.
+     * In formulas:
+     * \f[
+     * \alpha := (K + \sigma I)^{-1}\bar{y}_{obs}
+     * \f]
+     * The covariance vector is noised, i.e. the result of #computeNoisedCov.
+     * The normalized target observation vector is used.
+     *
+     * @param alpha Returns the (pre-)computed alpha value, size [getN()].
+     */
     void getAlpha(Eigen::VectorXd& alpha) const;
+
     //void getNoisedInvCov(Eigen::MatrixXd& ICov) const;
-    void getNoisedInvCov(Eigen::MatrixXd& ICov, const Eigen::MatrixXd& B) const;
+
+    /**
+     * Compute the product of the inverse covariance matrix and a given matrix B.
+     * Mathematically, it returns the solution of the following system of equations:
+     * \f[
+     * (K + \sigma I) X = B
+     * \f]
+     * The covariance vector is noised, i.e. the result of #computeNoisedCov.
+     *
+     * @param X Returns the computed product
+     * @param B The RHS of the above system of equations. Size [getN(), k], where k is arbitrary.
+     */
+    void getNoisedInvCov(Eigen::MatrixXd& X, const Eigen::MatrixXd& B) const;
+
+    /**
+     * Compute the logarithm of the determinant of the noised covariance matrix.
+     * \f[
+     * \log\left(\det\left(K + \sigma I\right)\right)
+     * \f]
+     *
+     * @return The logarithm of the determinant of the noised covariance matrix.
+     */
     double computeNoisedLogDetCov() const;
 
-    // Getter/Setter
+
+    /*
+    ********* getter and setter *********
+    */
+
+    /**
+     * Set the parameters of this GPKernel object as a parameter vector.
+     * The parameters are taken from the params vector starting at the given index.
+     *
+     * @param params The vector containing the new parameters for this object.
+     * @param index The index at which to start taking parameters from the given vector.
+     */
     void setParameters(const Eigen::VectorXd& params, const unsigned int index=0);
+
+    /**
+     * Get the parameters of this GPKernel object as a parameter vector.
+     * The parameters are written into the params vector starting at the given index.
+     * The size of the provided parameter vector must be at least index+nParameters().
+     *
+     * @param params Returns the new parameters for this object.
+     * @param index The index at which to start taking parameters from the given vector.
+     */
     void getParameters(Eigen::VectorXd& params, const unsigned int index=0) const;
+
+    /**
+     * Get the number of parameters of this object.
+     *
+     * @return The number of parameters.
+     */
     unsigned int nParameters() const;
+
+    /**
+     * Get a pointer to the currently registered GPData object.
+     *
+     * @return The currently registered GPData object.
+     */
+    std::shared_ptr<GPData> getData() const;
+
+    /**
+     * Get the number of data points in the registered GPData object.
+     * This number determines the size of the covariane matrix.
+     *
+     * @return The number of data points n and the size of the quadratic covariance matrix.
+     */
+    int getN() const;
 
 private:
     void precomputeAlpha();
