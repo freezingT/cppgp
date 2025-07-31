e4f7b909a06da6680f1bee8a3629e2025302cc4d
Christoph <c-alias-mail@posteo.de>
2025-07-31 14:15:06 +0200

    added documentation for prototype

--- cppgp/util/prototype.hpp
+++ cppgp/util/prototype.hpp
@@ -4,12 +4,22 @@
 
 namespace util
 {
-
+    /**
+     * Prototype interface class, implementing the Prototype design pattern.
+     * It defines a method that allows the creation of a deep copy of an object.
+     *
+     * @see https://refactoring.guru/design-patterns/prototype
+     */
     class Prototype {
     public:
         Prototype();
         virtual ~Prototype();
 
+        /**
+         * Create a deep copy of the object.
+         *
+         * @return The deep copy.
+         */
         virtual std::shared_ptr<Prototype> copy() const = 0;
     };
 
